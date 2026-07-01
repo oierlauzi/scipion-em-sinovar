23b016ab67cd4ef06315f25f90b62a35aab99982
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-07-01 23:37:45 +0200

    Added annotate

--- sinovar/protocols/__init__.py
+++ sinovar/protocols/__init__.py
@@ -3,5 +3,6 @@
 # Module to declare protocols
 # Find documentation here: https://scipion-em.github.io/docs/docs/developer/creating-a-protocol
 # **************************************************************************
+from .protocol_annotate import SinovarAnnotate
 from .protocol_distance import SinovarDistance
 from .protocol_embed import SinovarEmbed
--- sinovar/protocols/protocol_annotate.py
+++ sinovar/protocols/protocol_annotate.py
@@ -32,74 +32,67 @@ from pyworkflow.protocol.constants import LEVEL_ADVANCED
 import pyworkflow.protocol.params as params
 from pyworkflow.utils import Message
 
-from pwem.objects import SetOfParticles, Particle, SetOfParticlesFlex, ParticleFlex
+from pwem.objects import (
+    Integer, 
+    SetOfParticles, Particle, 
+    SetOfParticlesFlex, ParticleFlex
+)
 from pwem.protocols import EMProtocol
-from emtable import Table
 
 from sinovar import Plugin
-from sinovar.constants import SINOVAR
-from .protocol_distance import SinovarDistance
+
+from emtable import Table
+from relion.convert import writeSetOfParticles
 
 OUTPUT_PARTICLES = 'particles'
 
-class SinovarEmbed(EMProtocol):
-    _label = 'embed'
+def _writeSinovarEmbedding(particle: ParticleFlex, row: dict):
+    row['sinovarEmbedding'] = '[' + str(particle._zFlex.get()) + ']'
+
+class SinovarAnnotate(EMProtocol):
+    _label = 'annotate'
     _devStatus = BETA
     _possible_outputs = {
-        OUTPUT_PARTICLES: SetOfParticlesFlex
+        OUTPUT_PARTICLES: SetOfParticles
     }
     
     def _defineParams(self, form: params.Form):
         form.addSection(label=Message.LABEL_INPUT)
 
-        form.addParam('distanceProtocol', params.PointerParam,
-                      pointerClass = SinovarDistance,
+        form.addParam('inputParticles', params.PointerParam,
+                      pointerClass = SetOfParticlesFlex,
                       important = True,
-                      label = 'Distance protocol',
+                      label = 'Flex particles',
                       allowsNull = False,
                       )
         
-        form.addParam('components', params.IntParam,
-                      label='Number of embedding components',
-                      default=8 )
-        form.addParam('sigmaNn', params.IntParam,
-                      label='Number of NN used to estimate sigma',
-                      default=16 )
-        form.addParam('affinityNn', params.IntParam,
-                      label='Number of NN used for the affinity matrix',
-                      default=4096 )
-
     # --------------------------- STEPS functions ------------------------------
 
     def _insertAllSteps(self):
+        self._insertFunctionStep(self.convertInputStep)
         self._insertFunctionStep(self.runSinovarStep)
         self._insertFunctionStep(self.createOutputStep)
 
+    def convertInputStep(self):
+        writeSetOfParticles(
+            self.inputParticles.get(), 
+            self._getInputStarFilename(),
+            postprocessImageRow=_writeSinovarEmbedding
+        )
+    
     def runSinovarStep(self):
         program = 'sinovar'
 
-        args = ['embed']
+        args = ['annotate']
         args += ['-i', self._getInputStarFilename()]
-        args += ['-d', self._getDistanceMatrixFilename()]
         args += ['-o', self._getOutputStarFilename()]
-        args += ['--components', self.components.get()]
-        args += ['--sigma_k', self.sigmaNn.get()]
-        args += ['--affinity_k', self.affinityNn.get()]
-
-        gpus = self.getGpuList()
-        if gpus:
-            args += ['--device']
-            args += list(map('gpu:{:d}'.format, gpus))
             
         Plugin.runSinovar(self, program, args)
         
     def createOutputStep(self):
-        inputParticles: SetOfParticles = \
-            self._getDistanceProtocol().inputParticles.get()
-        outputParticles: SetOfParticlesFlex = SetOfParticlesFlex.create(
-            self._getPath(),
-            progName=SINOVAR
-        )
+        inputParticles: SetOfParticlesFlex = \
+            self.inputParticles.get()
+        outputParticles: SetOfParticles = SetOfParticles.create(self._getPath())
         outputParticles.copyInfo(inputParticles)
         
         star = Table()
@@ -107,31 +100,25 @@ class SinovarEmbed(EMProtocol):
             self._getOutputStarFilename(),
             tableName='particles'
         )
-        embeddings = star.getColumnValues('sinovarEmbedding')
+        classIds = star.getColumnValues('sinovarClassId')
         
-        particle: Particle
-        for particle, embedding in zip(inputParticles, embeddings):
-            flexParticle = ParticleFlex(SINOVAR)
-            flexParticle.copy(particle)
-            flexParticle.setZFlex(ast.literal_eval(embedding))
-            outputParticles.append(flexParticle)
+        particle: ParticleFlex
+        for particle, classId in zip(inputParticles, classIds):
+            outputParticle = Particle()
+            outputParticle.copy(particle)
+            outputParticle.setClassId(classId+1)
+            outputParticles.append(outputParticle)
         
         self._defineOutputs(**{OUTPUT_PARTICLES: outputParticles})
         self._defineSourceRelation(
-            self._getDistanceProtocol().inputParticles, 
+            self.inputParticles, 
             outputParticles
         )
-
+    
     # -------------------------- INFO functions --------------------------------
     # -------------------------- UTILS functions -------------------------------
-    def _getDistanceProtocol(self) -> SinovarDistance:
-        return self.distanceProtocol.get()
-        
     def _getInputStarFilename(self) -> str:
-        return self._getDistanceProtocol()._getInputStarFilename()
-
-    def _getDistanceMatrixFilename(self) -> str:
-        return self._getDistanceProtocol()._getDistanceMatrixFilename()
+        return self._getExtraPath('particles.star')
 
     def _getOutputStarFilename(self) -> str:
-        return self._getExtraPath('embedding.star')
+        return self._getExtraPath('annotated.star')
